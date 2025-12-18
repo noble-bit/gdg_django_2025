e3b6f8095eb0ddd6ff1c2d600da59a55611b569a
Noble <nebilfentaw864@gmail.com>
2025-12-18 12:00:26 +0300

    Add todo class and todo class skeleton

--- to_do/todo.py
+++ to_do/todo.py
@@ -39,3 +39,33 @@ class TodoApp:
                 indent = 4
             )
     
+    def add(self, title):
+        task_id = len(self.todos) + 1
+        self.todos.append(Todo(task_id, title))
+        self.save_todos()
+    
+    def view_tasks(self):
+        if not self.todos:
+            print("No tasks.")
+            return
+
+        for todo in self.todos:
+            status = "Done" if todo.completed else "Not Done"
+            print(f"{todo.id}. {todo.title} - {status}")
+
+    def complete_todo(self, task_id):
+        for todo in self.todos:
+            if todo.id == task_id:
+                todo.completed = True
+                self.save_todos()
+                return
+        print("Task not found.")
+
+        
+    def delete(self, task_id):
+        self.todos = [todo for todo in self.todos if todo.id != task_id]
+        self.save_todos()
+
+
+
+        
