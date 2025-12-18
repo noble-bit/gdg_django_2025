958744dc38d49b3c0c67f767a622a0c2720068f4
Noble <nebilfentaw864@gmail.com>
2025-12-18 14:04:21 +0300

    Add CLI menu and user interaction loop

--- to_do/todo.py
+++ to_do/todo.py
@@ -2,18 +2,19 @@ import os
 import json
 
 class Todo:
-    def __init__(self, title, task_id, completed=False):
-        
-        self.id = task_id
-        self.completed = completed
-        self.title = title
+    
+    def __init__(self, task_id, title, completed=False):
+            self.id = task_id
+            self.title = title
+            self.completed = completed
 
     def to_dict(self):
         
         return {
             "id": self.id,
+            "title": self.title,
             "completed": self.completed,
-            "title": self.title
+            
         }
     
 class TodoApp:
@@ -28,22 +29,18 @@ class TodoApp:
                 data = json.load(f)
                 for item in data:
                     self.todos.append(
-                        Todo(item["id"],item["completed"],item["title"])
+                        Todo(item["id"], item["title"], item["completed"])
                     )
 
     def save_todos(self):
         with open(self.filename, "w") as f:
-            json.dump(
-                [todo.to_dict() for todo in self.todos],
-                f,
-                indent = 4
-            )
-    
-    def add(self, title):
+            json.dump([todo.to_dict() for todo in self.todos], f, indent=4)
+
+    def add_todo(self, title):
         task_id = len(self.todos) + 1
         self.todos.append(Todo(task_id, title))
         self.save_todos()
-    
+
     def view_tasks(self):
         if not self.todos:
             print("No tasks.")
@@ -61,11 +58,41 @@ class TodoApp:
                 return
         print("Task not found.")
 
-        
-    def delete(self, task_id):
+    def delete_todo(self, task_id):
         self.todos = [todo for todo in self.todos if todo.id != task_id]
         self.save_todos()
 
+my_todo_app = TodoApp()
+
+while True:
+    print("\n1. Add Todo")
+    print("2. View Todos")
+    print("3. Mark as Completed")
+    print("4. Delete Todo")
+    print("5. Exit")
+
+    choice = input("Choose: ")
+
+    if choice == "1":
+        title = input("Enter task title: ")
+        my_todo_app.add_todo(title)
+
+    elif choice == "2":
+        my_todo_app.view_tasks()
+
+    elif choice == "3":
+        task_id = int(input("Enter task ID: "))
+        my_todo_app.complete_todo(task_id)
+
+    elif choice == "4":
+        task_id = int(input("Enter task ID: "))
+        my_todo_app.delete_todo(task_id)
+
+    elif choice == "5":
+        break
+
+    else:
+        print("Invalid choice")
 
 
         
